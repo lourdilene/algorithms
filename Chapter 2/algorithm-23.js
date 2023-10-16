29c594b0114fa4ccd5e4859abc6c6e13c9d2f518
Lourdilene Souza <lourdilene.souza@gmail.com>
2023-10-16 09:52:20 -0300

    algorithm-26 chapter-2

---
+++ Chapter 2/algorithm-23.js
@@ -0,0 +1,6 @@
+/* Algorithm 23.
+Enter an integer with 3 places and print the digit in the tens place.*/
+
+numer_3_digits = prompt("Enter a number of 3 digits and print tens place.");
+decimal_place_number = (numer_3_digits % 100) / 10;
+alert("Digit in the decimal place is: " + parseInt(decimal_place_number));
