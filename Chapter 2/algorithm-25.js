121d1a67b4671c6fa0cafdf9e135dbf8db6be771
Lourdilene Souza <lourdilene.souza@gmail.com>
2023-10-18 17:19:50 -0300

    algorithms 25 and 31

---
+++ Chapter 2/algorithm-25.js
@@ -0,0 +1,10 @@
+/* Algorithm 25.
+Enter a date in ddmmyy format and print: separate day, month and year..*/
+
+date = prompt("Enter a date in format ddmmaa");
+day = date / 10000;
+month = (date % 10000) / 100;
+year = (date % 1000) % 100;
+console.log("day is: " + parseInt(day));
+console.log("month is: " + parseInt(month));
+console.log("year is: " + parseInt(year));
---
+++ Chapter 2/algorithm-31.js
@@ -0,0 +1,5 @@
+/* Algorithm 31.
+Print the average arithmetic.*/
+
+arithmetic_average = (8 + 9 + 7) / 3;
+console.log("The arithmetic average is: " + arithmetic_average);
