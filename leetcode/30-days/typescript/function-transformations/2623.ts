cf214d85fb3b84298e5edf3e0cab9af7fccaa0e5
Lourdilene Souza <lourdilene.souza@gmail.com>
2023-12-12 19:36:59 -0300

    function-transformations map

---
+++ leetcode/30-days/typescript/function-transformations/2623.ts
@@ -0,0 +1,27 @@
+type Fn = (...params: number[]) => number;
+
+function memoize(fn: Fn): Fn {
+  const cache: Map<string, number> = new Map();
+
+  return function (...args: number[]) {
+    const key = args.toString();
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+}
+
+/**
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1
+ */
