3aa1e42a137aeca4b82cf08c60eb5d2567d1432e
Lourdilene Souza <lourdilene.souza@gmail.com>
2025-01-10 20:04:07 -0300

    study lists

---
+++ python-crash-course/guest_list.py
@@ -0,0 +1,10 @@
+guests = ["Maria", "João", "Ana", "Beatriz", "Pedro"]
+print(guests)
+guests.insert(0,"Lourdilene")
+print(guests)
+guests.append("Joana")
+print(guests)
+guests.pop()
+print(guests)
+guests.remove("Lourdilene")
+print(guests)
